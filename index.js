4c5d9c22c8f07a1f637caf81ce00b603ba3bf47a
Kush Vardhan <kushvardhan39797@gmail.com>
2026-01-29 22:00:40 +0530

    chore: add initial documentation and update package.json scripts

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
-app.get("/test",(req,res)=>{
-    return res.json({
-        msg:"Eelalala Aae u"
-    });
+app.get("/test", (req, res) => {
+  return res.json({
+    msg: "Eelalala Aae u",
+  });
 });
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server running on PORT: ${process.env.PORT}`);
+app.get("/home", (req, res) => {
+  return res.json({
+    msg: "HOME COMMING",
+  });
+});
+
+app.get("/info", (req, res) => {
+  return res.json({
+    msg: "INFO ROUTE",
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on PORT: ${process.env.PORT}`);
 });
